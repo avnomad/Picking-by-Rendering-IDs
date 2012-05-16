fbad6ecaef2b137c7fef0dc3b5a195f93158328f
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-05-17 02:04:22 +0300

    Added copyright notices and license.

--- Picking by Rendering IDs/main.cpp
+++ Picking by Rendering IDs/main.cpp
@@ -1,3 +1,21 @@
+//	Copyright (C) 2010-2012 Vaptistis Anogeianakis <el05208@mail.ntua.gr>
+/*
+ *	This file is part of Picking by Rendering IDs.
+ *
+ *	Picking by Rendering IDs is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	Picking by Rendering IDs is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with Picking by Rendering IDs.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <gl/glew.h>
 #include <gl/freeglut.h>
 #include <cstdlib>
